5f1f4b92b0f79785815021e9d8c2d6da6635abc8
meinardus <brya.seran@gmail.com>
2025-09-09 21:28:22 +0700

    update productions

--- kqms/models/ore_productions.py
+++ kqms/models/ore_productions.py
@@ -37,7 +37,6 @@ class OreProductions(models.Model):
     id_user             = models.IntegerField(default=None, null=True, blank=True)
     status_approval     = models.CharField(max_length=50, default=None, null=True, blank=True)
     category            = models.CharField(max_length=10, default=None, null=True, blank=True)
-    hauler_plan         = models.CharField(max_length=30, default=None, null=True, blank=True)
     direct              = models.CharField(max_length=15, default='No', null=True, blank=True)
     created_at          = models.DateTimeField(auto_now_add=True)
     updated_at          = models.DateTimeField(auto_now_add=True)
